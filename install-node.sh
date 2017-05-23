0acd09953fa52c28a13b320cd20166426ff40068
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-23 22:39:52 +0200

    fixed ypo in install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -5,4 +5,4 @@ sudo ufw allow 4500/udp
 sudo ufw allow 4789/udp
 
 ##### Register host in Rancher in Default environment - To be studied how to use API - !!!!WARNING!!! Agent version may change => check in Rancher GUI
-sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.2 http://192.168.99.100:8080
+sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.2 http://rancher.infra.local:8080
