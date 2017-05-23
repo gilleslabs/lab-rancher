3c32057360e7c37d0a1eba50f9f99b85495e1ca1
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-23 22:43:53 +0200

    fixed ypo in install-rancher.sh

--- install-rancher.sh
+++ install-rancher.sh
@@ -10,6 +10,6 @@ sudo ufw allow 4789/udp
 sudo docker run -d --restart=unless-stopped -e CATTLE_PROMETHEUS_EXPORTER=true -p 8080:8080 -p 9108:9108 rancher/server
 
 ###### Enabling Rancher-Server to run containers - !!!!WARNING!!! Agent version may change => check in Rancher GUI
-sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.2 http://10.200.194.100
+sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.2 http://10.200.194.100:8080
 
  
