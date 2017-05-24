fcbd77bd6bec829504e128d107dd53eb0d61c423
gilleslabs <gilles.labs.tosi@gmail.com>
2017-05-24 22:31:45 +0200

    removed agent from rancher server

--- install-rancher.sh
+++ install-rancher.sh
@@ -7,7 +7,3 @@ sudo ufw allow 4789/udp
 
 sudo docker run -d --restart=unless-stopped -e CATTLE_PROMETHEUS_EXPORTER=true -p 8080:8080 -p 9108:9108 rancher/server
 
-###### Enabling Rancher-Server to run containers - !!!!WARNING!!! Agent version may change => check in Rancher GUI
-sudo docker run -d --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher rancher/agent:v1.2.2 http://rancher.infra.local:8080
-
- 
